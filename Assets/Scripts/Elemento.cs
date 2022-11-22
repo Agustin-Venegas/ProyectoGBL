55974b76b0614bb0127cafd6f533b6e597147358
ZergV <avenegas19@alumnos.utalca.cl>
2022-11-22 12:05:36 -0400

    primer commit
    se me habia olvidado q hay que subir cosas

---
+++ Assets/Scripts/Elemento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//esta clase se encarga de representar un elemento que se puede poner en la pantalla
+//tiene que tener un objeto tipo info que se puede editar y eso
+//spawnea como prefab cuando se hace funcionar un boton con el mouse y eso
+
+public class Elemento : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/VisorElementos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//esta clase se encarga de obtener todos los elementos de un JSON
+//y mostrarlos en un elemento estilo scrollbar
+
+public class VisorElementos : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
