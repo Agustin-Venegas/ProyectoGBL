ecefa1659133ee8127bdd4ff002677b1b04c527c
Galadriel González <galakatana@gmail.com>
2022-11-25 18:58:06 -0400

    TareasGalaSemana!

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveData : MonoBehaviour
+{
+    string path;
+
+    public void CreateDataFile()
+    {
+        path = Application.dataPath + "/saveGameData.txt";
+
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path, "SavedGameData \n\n");
+        }
+    }
+
+    public void Start()
+    {
+        CreateDataFile();
+        SaveInfo("Pichula" +
+            " wena conchetumare" +
+            " probando probando");
+        LoadInfo();
+    }
+
+    public void SaveInfo(string infoToSave)
+    {
+        File.AppendAllText(path, infoToSave);
+    }
+
+    public void LoadInfo()
+    {
+        StreamReader reader = new StreamReader(path);
+        Debug.Log(reader.ReadToEnd());
+        reader.Close();
+    }
+
+}
