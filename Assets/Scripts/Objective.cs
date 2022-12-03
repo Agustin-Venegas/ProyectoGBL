f5e5f6bc8471547b0fb03b0f04c55de63d202d50
Galadriel González <galakatana@gmail.com>
2022-12-02 20:29:58 -0400

    Objective Manager

---
+++ Assets/Scripts/Objective.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Objective
+{
+    public bool isCompleted;
+    public int currentCompleted;
+    public int maxCompleted;
+    public float percentageCompleted;
+    public string objectiveDescription;
+
+    public Objective(string Description, int maxValue) //Constructor
+    {
+        this.maxCompleted = maxValue;
+        this.objectiveDescription = Description;
+    }
+    
+    public void IncrementValue() //Incrementar valor al progreso del objetivo
+    {
+        currentCompleted++;
+        percentageCompleted = currentCompleted / maxCompleted;
+        if(currentCompleted >= maxCompleted)
+        {
+            isCompleted = true;
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/ObjectiveList.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ObjectiveList : MonoBehaviour
+{
+
+    [SerializeField]
+    List<Objective> objectiveList = new List<Objective>();
+
+    public int completedObjectives;
+    public bool completedGoal;
+
+    public float levelPercentageCompleted;
+    public int currentObjectiveIndex;
+    public TextMeshProUGUI objectiveUIText;
+
+    public void Update()
+    {
+        objectiveUIText.text = objectiveList[currentObjectiveIndex].objectiveDescription;
+    }
+
+    public void IncrementIndexCurrentObjective()
+    {
+        if (currentObjectiveIndex < objectiveList.Count)
+        {
+            currentObjectiveIndex++;
+        }
+    }
+
+    public void checkCompleted() //Comprobar completitud del nivel o meta final
+    {
+        foreach(var obj in objectiveList)
+        {
+            if(obj.isCompleted)
+            {
+                completedObjectives++;
+            }
+        }
+
+        levelPercentageCompleted = completedObjectives / (objectiveList.Count + 1);
+
+        if(completedObjectives - 1 >= objectiveList.Count)
+        {
+            completedGoal = true;
+        }
+    }
+  
+    public void AddObjective(int maxValue, string description) //Añadir un objetivo nuevo a la lista
+    {
+        objectiveList.Add(new Objective(description, maxValue));
+    }
+
+    public void RemoveObjectiveByDescription(string description) //Eliminar objetivo por descripción
+    {
+        foreach(var obj in objectiveList)
+        {
+            if(description == obj.objectiveDescription)
+            {
+                objectiveList.Remove(obj);
+            }
+        }
+    }
+
+    public void RemoveObjectiveByIndex(int indexToRemove) //Eliminar objetivo por índice en la lista
+    {
+        objectiveList.RemoveAt(indexToRemove);
+    }
+}
