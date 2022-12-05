e6b5ef6f3a9ca33161f9138ad472bf73500a1fe4
Galadriel González <galakatana@gmail.com>
2022-12-05 14:14:29 -0400

    Panel modificador de objetos con script elemento

---
+++ Assets/Scripts/ElementModifier.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ElementModifier : MonoBehaviour
+{
+    public GameObject panelModifier;
+    GameObject ScaleSlider;
+    GameObject RotateSlider;
+
+    public bool isSelected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //panelModifier = GameObject.FindGameObjectWithTag("PanelModifier");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnMouseDown()
+    {
+        panelModifier = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<FindAllElements>().panelMod;
+        panelModifier.SetActive(true);
+        ScaleSlider = GameObject.FindGameObjectWithTag("ScaleSlider");
+        RotateSlider = GameObject.FindGameObjectWithTag("RotateSlider");
+
+        isSelected = true;
+        if (this.gameObject.GetComponent<Elemento>())
+        {
+            Debug.Log("HasElement");
+        }
+
+        ScaleSlider.GetComponent<Slider>().value = this.gameObject.transform.localScale.x;
+        RotateSlider.GetComponent<Slider>().value = this.gameObject.transform.rotation.z;
+    }
+
+    public void SaveElementModification()
+    {
+
+        this.gameObject.transform.localScale = new Vector3(ScaleSlider.GetComponent<Slider>().value, ScaleSlider.GetComponent<Slider>().value, ScaleSlider.GetComponent<Slider>().value);
+        this.gameObject.transform.rotation = new Quaternion(0, 0, RotateSlider.GetComponent<Slider>().value, 0);
+    }
+
+
+}
---
+++ Assets/Scripts/FindAllElements.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindAllElements : MonoBehaviour
+{
+    public GameObject panelMod;
+    private void Start()
+    {
+        panelMod = GameObject.FindGameObjectWithTag("PanelModifier");
+        panelMod.SetActive(false);
+    }
+
+    //GameObject elementSelected[];
+
+    public void FindSelectedElement()
+    {
+        var elementSelected = GameObject.FindGameObjectsWithTag("Elemento");
+
+        foreach (var element in elementSelected)
+        {
+            if (element.GetComponent<ElementModifier>().isSelected)
+            {
+                element.GetComponent<ElementModifier>().panelModifier = panelMod;
+                element.GetComponent<ElementModifier>().SaveElementModification();
+                element.GetComponent<ElementModifier>().panelModifier.SetActive(false);
+                element.GetComponent<ElementModifier>().isSelected = false;
+            }
+        }
+    }
+}
